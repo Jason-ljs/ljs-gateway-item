65d1dd5f577b352afae26a874de07e74833ae5c0
Jason-ljs <18931984300@163.com>
2019-08-04 21:15:10 +0800

    Initial commit

---
+++ ljs-gateway-server/src/main/java/com/ljs/GateWayServerApplication.java
@@ -0,0 +1,19 @@
+package com.ljs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName GateWayServerApplication
+ * @Description: TODO
+ * @Author 小松
+ * @Date 2019/8/4
+ **/
+@SpringBootApplication
+public class GateWayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayServerApplication.class);
+    }
+
+}
